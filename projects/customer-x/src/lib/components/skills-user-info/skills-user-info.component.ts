a7422241c5d1c78049158b7191b98b3c697156b7
mirkorap16 <m.rapisarda@acadevmy.it>
2025-09-16 14:13:44 +0200

    Refactor HTML and TypeScript for user info display
    - Updated `skills-user-info.component.ts` template for better readability with formatted HTML.
    - Enhanced `details-page.component.html` by adjusting class names for improved styling and hover effects, ensuring a consistent user experience.
    - Changed table header classes to use a unified background color and text styling for better visibility.
    - Updated table row hover effects to match the new design scheme.

--- projects/customer-x/src/lib/components/skills-user-info/skills-user-info.component.ts
+++ projects/customer-x/src/lib/components/skills-user-info/skills-user-info.component.ts
@@ -4,7 +4,9 @@ import { Plugin } from '@devmy/decorators/plugin.decorator';
 @Plugin({ groupId: 'details-page' })
 @Component({
   selector: 'app-skills-user-info',
-  template: `<div class="flex justify-between items-center py-3 border-b border-gray-200">
+  template: `<div
+    class="flex justify-between items-center py-3 border-b border-gray-200"
+  >
     <span class="text-sm font-semibold tracking-wider text-gray-600 uppercase">
       Skills:
     </span>
